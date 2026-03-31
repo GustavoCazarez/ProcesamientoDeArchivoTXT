86587da3e98f0d15ace98a69039fbac54f0a9831
ManuelPaezzz <117866813+ManuelPaezzz@users.noreply.github.com>
2026-03-31 14:45:33 -0700

    Update analisis.py

--- analisis.py
+++ analisis.py
@@ -1,8 +1,6 @@
 """
-PROYECTO: Análisis Exploratorio de Datos Climatológicos - Estación Acatitán
-LIBRERÍAS UTILIZADAS: 
-- Pandas: Para la manipulación de estructuras de datos tabulares.
-- Numpy: Para el manejo de valores nulos y operaciones matemáticas.
+Procesamiento de 
+archivo Txt 
 """
 
 import pandas as pd
@@ -17,14 +15,14 @@ def cargar_datos(ruta_archivo):
 def limpiar_clima(df):
     """Realiza la limpieza, conteo de faltantes e imputación por media."""
     
-    # 1. Limpieza de Fechas: Convertimos y eliminamos filas de texto basura (guiones)
+    #limpieza de Fechas
     df['FECHA'] = pd.to_datetime(df['FECHA'], dayfirst=True, errors='coerce')
     df = df.dropna(subset=['FECHA'])
     
-    # 2. Conteo de faltantes: Se crea columna sumando nulos antes de rellenar
+    # se crea columna sumando nulos antes de rellenar
     df['TOTAL_FALTANTES'] = df[['PRECIP', 'EVAP', 'TMAX', 'TMIN']].isnull().sum(axis=1)
     
-    # 3. Imputación: Rellenar nulos con la media para mantener la consistencia estadística
+    # rellenar nulos con la media para mantener la consistencia estadística
     for col in ['PRECIP', 'EVAP', 'TMAX', 'TMIN']:
         df[col] = pd.to_numeric(df[col], errors='coerce')
         df[col] = df[col].fillna(df[col].mean())
@@ -35,17 +33,17 @@ def ejecutar_analisis():
     """Función principal que orquesta el proceso."""
     archivo = '25001.txt'
     
-    # Paso 1: Carga
+    #Carga
     raw_data = cargar_datos(archivo)
     
-    # Paso 2: Limpieza y Procesamiento
+    #Limpieza y Procesamiento
     df_limpio = limpiar_clima(raw_data)
     
-    # Paso 3: Mostrar resultados (Ejemplos)
+    #mostrar resultados
     print("Muestra de datos procesados (Primeras 5 filas):")
     print(df_limpio.head())
     
-    # Paso 4: Exportación
+    #Exportación
     df_limpio.to_csv('datos_procesados_acatitan.csv', index=False)
     print("\nÉxito: Se ha generado el archivo 'datos_procesados_acatitan.csv'")
 
